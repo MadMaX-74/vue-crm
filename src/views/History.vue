027a72b4a3db50fa54d56cc9c9fdc59b31fa6434
MaksimMaltsev <m.maltsev@dsec.com>
2022-11-27 01:28:47 +0300

    add diagram to history page

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <PieChart :lable="categories" :pieData="categoriesData"/>
     </div>
 
     <Loader v-if="loading"/>
@@ -31,28 +31,45 @@
 import Vue from "vue";
 import HistoryTable from "@/components/HistoryTable.vue";
 import paginationMixin from "@/mixins/pagination.mixin.js";
+import PieChart from "@/components/app/Pie.vue";
 
 export default Vue.extend({
   name: 'history',
   mixins: [paginationMixin],
-  components: {HistoryTable},
+  components: {PieChart, HistoryTable},
   data: () => ({
     loading: true,
-    records : []
+    records : [],
+    categories: [],
+    categoriesData: []
   }),
   async mounted () {
     this.records = await this.$store.dispatch('fetchRecords')
     const categories = await this.$store.dispatch('fetchCategory')
+    this.setup(categories)
+    this.categories = categories.map(c => c.title)
+    this.categoriesData = categories.map(c => {
+      return this.records.reduce((total, r) => {
+        if (r.categoryId === c.id && r.type === 'outcome'){
+          total += +r.amount
+        }
+        return total
+      }, 0)
+    })
 
-    this.setupPagination(this.records.map(record => {
-      return {
-        ...record,
-        categoryName: categories.find(c => c.id === record.categoryId).title,
-        typeClass: record.type === 'income' ? 'green' : 'red',
-        typeText: record.type === 'income' ? 'Доход' : 'Расход'
-      }
-    }))
     this.loading = false
+  },
+  methods: {
+    setup (categories) {
+      this.setupPagination(this.records.map(record => {
+        return {
+          ...record,
+          categoryName: categories.find(c => c.id === record.categoryId).title,
+          typeClass: record.type === 'income' ? 'green' : 'red',
+          typeText: record.type === 'income' ? 'Доход' : 'Расход'
+        }
+      }))
+    }
   }
 })
 </script>
